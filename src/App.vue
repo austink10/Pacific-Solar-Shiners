59ab3778a8884d8577976a7c50ae0fc03fba14cf
Austin Kirby <austinkirby@MacBook-Air-3.local>
2025-12-27 19:23:15 -0500

    Move About link to end of navigation after Partnerships

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@
           </router-link>
           <nav class="nav">
             <router-link to="/" class="nav-link">Home</router-link>
-            <router-link to="/about" class="nav-link">About</router-link>
             <router-link to="/services" class="nav-link">Services</router-link>
             <router-link to="/projects" class="nav-link">Projects</router-link>
             <router-link to="/case-studies" class="nav-link">Case Studies</router-link>
             <router-link to="/partnerships" class="nav-link">Partnerships</router-link>
+            <router-link to="/about" class="nav-link">About</router-link>
           </nav>
           <button class="mobile-menu-toggle" @click="toggleMobileMenu" aria-label="Toggle menu">
             <span></span>
@@ -22,11 +22,11 @@
         </div>
         <nav class="mobile-nav" :class="{ 'is-open': isMobileMenuOpen }">
           <router-link to="/" class="nav-link" @click="closeMobileMenu">Home</router-link>
-          <router-link to="/about" class="nav-link" @click="closeMobileMenu">About</router-link>
           <router-link to="/services" class="nav-link" @click="closeMobileMenu">Services</router-link>
           <router-link to="/projects" class="nav-link" @click="closeMobileMenu">Projects</router-link>
           <router-link to="/case-studies" class="nav-link" @click="closeMobileMenu">Case Studies</router-link>
           <router-link to="/partnerships" class="nav-link" @click="closeMobileMenu">Partnerships</router-link>
+          <router-link to="/about" class="nav-link" @click="closeMobileMenu">About</router-link>
         </nav>
       </div>
     </header>
